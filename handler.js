9f2a50e77fcdd20d19872e94a09c8dcb2b7d6e3a
Nicole <nicole.tomlinson92@gmail.com>
2020-02-02 12:07:13 +0000

    changes to some functionality

--- handler.js
+++ handler.js
@@ -19,7 +19,7 @@ const connection = mysql.createConnection({
 app.get("/developers", function (request, response) {
   // request is an object with lots of info about the request
   // response is an object which allows us to define what kind of response we want to send back
-  connection.query("SELECT * FROM developers", function (err, data) {
+  connection.query(`SELECT * FROM developers`, function (err, data) {
     if (err) {
       response.status(500).json({
         error: err
@@ -35,7 +35,7 @@ app.get("/developers", function (request, response) {
 // POST /developers
 app.post("/developers", function (request, response) {
   const newDeveloper = request.body;
-  connection.query("INSERT INTO developers SET ?", [newDeveloper], function (err, data) {
+  connection.query(`INSERT INTO developers SET ?`, [newDeveloper], function (err, data) {
     if (err) {
       response.status(500).json({ error: err });
     } else {
@@ -50,7 +50,7 @@ app.put("/developers/:id", function (request, response) {
   const updatedDeveloper = request.body;
   const id = request.params.id;
   // escaping queries 
-  connection.query('UPDATE developers SET ?  WHERE id= ?', [updatedDeveloper, id],
+  connection.query(`UPDATE developers SET ?  WHERE id= ?`, [updatedDeveloper, id],
     function (err) {
       if (err) {
         response.status(500).json({ error: err });
@@ -64,7 +64,7 @@ app.put("/developers/:id", function (request, response) {
 // DELETE /developers
 app.delete("/developers/:id", function (request, response) {
   const id2 = request.params.id;
-  connection.query('DELETE FROM developers WHERE id=?', [id2], function (err) {
+  connection.query(`DELETE FROM developers WHERE id=?`, [id2], function (err) {
     if (err) {
       response.status(500).json({
         error: err
